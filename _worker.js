c382d8aaafde8367910835d63cd9f675b31515de
HiinEnkelte <140715298+XyHK-HUC@users.noreply.github.com>
2024-12-10 03:34:04 +0800

    更新API接口

--- _worker.js
+++ _worker.js
@@ -51,7 +51,7 @@ export default {
   }
 };
 async function fetchAndDecryptData() {
-  const apiUrl = 'https://application.xyhk.us.kg/serverlist';
+  const apiUrl = 'https://web.enkelte.ggff.net/api/serverlist';
   const headers = { 'accept': '/', 'appversion': '1.3.1', 'user-agent': 'SkrKK/1.3.1', 'content-type': 'application/x-www-form-urlencoded' };
   const key = new TextEncoder().encode('65151f8d966bf596');
   const iv = new TextEncoder().encode('88ca0f0ea1ecf975');
